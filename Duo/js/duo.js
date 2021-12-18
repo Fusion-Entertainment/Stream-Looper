9e745695c9ccd439d11150ee917b3d4c6a8c6958
TheApexDevil <44646723+TheApexDevil@users.noreply.github.com>
2021-12-18 19:13:46 +0100

    Twtich Test

---
+++ Duo/js/duo.js
@@ -0,0 +1,7 @@
+new Twitch.Embed("twitch-embed", {
+    width: 1920,
+    height: 1080,
+    channel: "lana_lux",
+    // Only needed if this page is going to be embedded on other websites
+    parent: ["embed.example.com", "othersite.example.com"]
+  });
